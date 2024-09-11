e9fb9b3270a018021ff715ab7b61b755c4eaa4ca
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2024-09-11 19:43:51 +0100

    Todo list project

--- scripts/Todo-list.js
+++ scripts/Todo-list.js
@@ -11,24 +11,36 @@ renderTodoList()
 function renderTodoList() {
     let todoListHTML = ``;
 
-    for (let i = 0; i < todoList.length; i++) {
-        const todoObject = todoList[i];
-        //const name = todoObject.name;
-        //const dueDate = todoObject.dueDate;
+    todoList.forEach((todoObject) => {
+        
         const {name, dueDate} = todoObject;
         const html = 
         `<div>${name}</div>
            <div>${dueDate}</div>
-            <button onclick="
-               todoList.splice(${i}, 1);
-               renderTodoList();
-            " class="delete-todo-button">Delete</button> 
+            <button 
+             class="delete-todo-button js-delete-todo-button">
+             Delete
+             </button> 
           `;
         todoListHTML += html;
-    }
+    });
+
 
      document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+     document.querySelectorAll('.js-delete-todo-button')
+     .forEach((deleteButton, index) => {
+         deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1);
+               renderTodoList();
+         })
+     });
 }
+
+document.querySelector('.js-add-todo-button')
+.addEventListener('click', () => {
+    addTodo();
+})
 
 function addTodo() {
    const inputElement =  document.querySelector('.js-name-input');
@@ -38,8 +50,6 @@ function addTodo() {
    const dueDate = dateInputElement.value;
 
    todoList.push({
-    //name: name,
-    //dueDate: dueDate,
     name, 
     dueDate
 });
--- scripts/rock-paper-scissor.js
+++ scripts/rock-paper-scissor.js
@@ -35,6 +35,51 @@ return computerMove;
 
 }
 
+let isAutoPlaying = false;
+let intervalId;
+
+//const autoPlay =() => {};
+
+function autoPlay() {
+   if(!isAutoPlaying) {
+    intervalId = setInterval(() => {
+        const playerMove = pickComputerMove();  
+        playGame(playerMove);
+      },1000);
+      isAutoPlaying = true;
+   } else {
+      clearInterval(intervalId);
+      isAutoPlaying = false;
+   }
+}
+
+document.querySelector('.js-rock-button')
+.addEventListener('click', () => {
+   playGame('rock');
+})
+
+document.querySelector('.js-paper-button')
+.addEventListener('click', () => {
+   playGame('paper');
+})
+
+document.querySelector('.js-scissors-button')
+.addEventListener('click', () => {
+   playGame('scissors');
+})
+
+
+document.body.addEventListener('keydown', (event) => {
+   if (event.key === 'r') {
+      playGame('rock');
+   } else if (event.key === 'p') {
+      playGame('paper');
+   } else if (event.key === 's') {
+      playGame('scissors');
+   }
+})
+
+
 function playGame(playerMove) {
 
 const computerMove = pickComputerMove();
