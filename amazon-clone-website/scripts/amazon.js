fa7c869ee0ca786bb8dcb8a6c49bdeb8d0542dea
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2024-09-12 22:54:20 +0100

    added product to the amazon website with js

--- amazon-clone-website/scripts/amazon.js
+++ amazon-clone-website/scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [{
-   image : 'images/products/athletic-cotton-socks-6-pairs.jpg', 
-   name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-   ratings : {
-    stars : 4.5,
-    count : 87
-   },
-   priceCents: 1090
-
-}, 
-{
-    image : 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    ratings : {
-        stars: 4,
-        count: 127,
-    },
-    priceCents: 2095
-},
-
-{
-    image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-    ratings : {
-        stars: 4.5,
-        count : 56
-    },
-    priceCents : 799
-}
-];
 
 let productHTML = '';
 
@@ -45,9 +15,9 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.ratings.stars * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-              ${product.ratings.count}
+              ${product.rating.count}
             </div>
           </div>
 
@@ -85,3 +55,5 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productHTML;
+
+console.log(productHTML)
