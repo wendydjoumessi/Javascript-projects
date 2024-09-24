89b9da6524a92eabc4b096d8ee9460502adc7fce
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2024-09-24 08:50:19 +0100

    Rename the jasmine folder to tests

--- amazon-clone-website/tests/tests-simple/moneyTest.js
+++ amazon-clone-website/tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 if(formatCurrency(2095) === '20.95') {
     console.log('passed');
