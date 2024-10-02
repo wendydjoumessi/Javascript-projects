0ab50fd279e5d4579f7e1c5c440cc46f55cd3483
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2024-10-02 09:43:46 +0100

    learn more about this class

--- amazon-clone-website/data/products.js
+++ amazon-clone-website/data/products.js
@@ -59,30 +59,13 @@ extraInfoHTML() {
 }
 
  }
-  const tshirt = new clothing({
-    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799,
-    keywords: [
-      "tshirts",
-      "apparel",
-      "mens"
-    ],
-    type: "clothing",
-    sizeChartLink: "images/clothing-size-chart.png"
-  });
-
-  console.log(tshirt);
-
-
-
-
  
+ /*
+const date = new Date();
+date.toLocaleTimeString();
+*/
+
+
  export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
