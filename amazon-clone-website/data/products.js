717aac1b4c64feea5492fcc724540ea6128d5d3b
Wendydjoumessi <133278891+wendydjoumessi@users.noreply.github.com>
2024-10-02 10:59:14 +0100

    load products from the backend

--- amazon-clone-website/data/products.js
+++ amazon-clone-website/data/products.js
@@ -65,7 +65,28 @@ const date = new Date();
 date.toLocaleTimeString();
 */
 
+export let products = [];
 
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+   xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new clothing(productDetails)
+      }
+      return new product(productDetails);
+    });
+    fun();
+   });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
  export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -731,4 +752,4 @@ date.toLocaleTimeString();
   }
   return new product(productDetails);
 });
-
+*/
